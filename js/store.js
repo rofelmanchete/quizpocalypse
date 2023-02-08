d099ac3347bd42c268b17b55c426a24e641ed3fa
kulotsystems <kulotsystems@gmail.com>
2023-02-09 01:00:03 +0800

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,17 +1,17 @@
 // modules
 import arvic from './modules/arvic.js';
-import jessa from './modules/jessa.js';
+
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
+import jessa from './modules/jessa.js';
 
 const store = Vuex.createStore({
     modules: {
         arvic,
-        jessa,
 
         // include your module here (e.g. john)
         // ...
-
+        jessa,
     }
 });
 
