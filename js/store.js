79a2164375cfd084efbc4d9a5877bd7b4f017505
imac15 <jersiguenza@my.cspc.edu.ph>
2023-02-08 16:58:13 +0800

    Add jessa.js

--- js/store.js
+++ js/store.js
@@ -1,12 +1,13 @@
 // modules
 import arvic from './modules/arvic.js';
-
+import jessa from './modules/jessa.js';
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
         arvic,
+        jessa,
 
         // include your module here (e.g. john)
         // ...
