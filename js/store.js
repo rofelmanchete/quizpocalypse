39d13b25a2ff93e1f40e4163fa27de5bae64c737
Maria Alexa Sharmaine Bermido <sharbermidovaldez@gmail.com>
2023-02-08 17:06:44 +0800

    create Quiz

--- js/store.js
+++ js/store.js
@@ -3,13 +3,14 @@ import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
+import alexa from './modules/alexa.js';
 
 const store = Vuex.createStore({
     modules: {
         arvic,
 
         // include your module here (e.g. john)
-        // ...
+        alexa,
 
     }
 });
