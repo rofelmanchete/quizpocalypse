d94132b4ab4e14fbe1f8874bc825f7d5ed449b98
Tammy Faye Sabas <tfayedlssabas@gmail.com>
2023-02-08 16:53:00 +0800

    Create Quiz

--- js/store.js
+++ js/store.js
@@ -2,14 +2,14 @@
 import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
-// ...
+import tammy from './modules/tammy.js'
 
 const store = Vuex.createStore({
     modules: {
         arvic,
 
         // include your module here (e.g. john)
-        // ...
+        tammy,
 
     }
 });
