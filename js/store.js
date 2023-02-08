d7058c6279bca0c026730856ca9c375817a4e1d9
Francisca Mae D. Apolinar <franciscaapolinar0306@gmail.com>
2023-02-08 16:54:58 +0800

    create quiz

--- js/store.js
+++ js/store.js
@@ -2,14 +2,16 @@
 import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
-// ...
+
+import francisca from './modules/francisca.js';
 
 const store = Vuex.createStore({
     modules: {
         arvic,
 
         // include your module here (e.g. john)
-        // ...
+        
+        francisca,
 
     }
 });
