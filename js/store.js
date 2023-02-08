1f716166933e9c4ba2ad26cbed79d894b12aef1b
Allyson Dioquino <allysondioquino@gmail.com>
2023-02-08 16:59:03 +0800

    create quiz

--- js/store.js
+++ js/store.js
@@ -2,14 +2,14 @@
 import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
-// ...
+import allyson from './modules/allyson.js';
 
 const store = Vuex.createStore({
     modules: {
         arvic,
 
         // include your module here (e.g. john)
-        // ...
+        allyson,
 
     }
 });
